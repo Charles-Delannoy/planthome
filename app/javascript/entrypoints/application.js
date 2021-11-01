4971ded233e1282d92f6ad299225a6c35915693a
charles delannoy <charles.delannoy@hei.fr>
2021-11-01 19:40:19 +0100

    replace wabpack by vite + React setup

--- app/javascript/entrypoints/application.js
+++ app/javascript/entrypoints/application.js
@@ -1,30 +1,19 @@
-// To see this message, add the following to the `<head>` section in your
-// views/layouts/application.html.erb
-//
-//    <%= vite_client_tag %>
-//    <%= vite_javascript_tag 'application' %>
-console.log('Vite ⚡️ Rails')
+// This file is automatically compiled by Webpack, along with any other files
+// present in this directory. You're encouraged to place your actual application logic in
+// a relevant structure within app/javascript and only use these pack files to reference
+// that code so it'll be compiled.
+//= require jquery
+//= require jquery_ujs
+require("@jquery/ujs").start()
+require("@rails/ujs").start()
+require("turbolinks").start()
+require("@rails/activestorage").start()
+require("channels")
 
-// If using a TypeScript entrypoint file:
-//     <%= vite_typescript_tag 'application' %>
-//
-// If you want to use .jsx or .tsx, add the extension:
-//     <%= vite_javascript_tag 'application.jsx' %>
 
-console.log('Visit the guide for more information: ', 'https://vite-ruby.netlify.app/guide/rails')
-
-// Example: Load Rails libraries in Vite.
+// Uncomment to copy all static images under ../images to the output folder and reference
+// them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
+// or the `imagePath` JavaScript helper below.
 //
-// import '@rails/ujs'
-//
-// import Turbolinks from 'turbolinks'
-// import ActiveStorage from '@rails/activestorage'
-//
-// // Import all channels.
-// import.meta.globEager('./**/*_channel.js')
-//
-// Turbolinks.start()
-// ActiveStorage.start()
-
-// Example: Import a stylesheet in app/frontend/index.css
-// import '~/index.css'
+// const images = require.context('../images', true)
+// const imagePath = (name) => images(name, true)
