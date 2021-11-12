1a10898f7cbebc488ed78d728dfb60bc6952eb8b
charles delannoy <charles.delannoy@hei.fr>
2021-11-12 11:22:10 +0100

    footer menu design

--- app/javascript/components/shared/Footer.jsx
+++ app/javascript/components/shared/Footer.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faHome, faTint, faSearch } from '@fortawesome/free-solid-svg-icons'
+import { faPagelines } from '@fortawesome/free-brands-svg-icons'
 
 const Header = () => {
   return (
     <div className='footer'>
-      <h1>Plant'Home</h1>
+      <div className='menu-btn'><FontAwesomeIcon icon={faHome} /></div>
+      <div className='menu-btn'><FontAwesomeIcon icon={faSearch} /></div>
+      <div className='menu-btn'><FontAwesomeIcon icon={faPagelines} /></div>
+      <div className='menu-btn'><FontAwesomeIcon icon={faTint} /></div>
     </div>
   )
 }
