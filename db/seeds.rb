51c25e1c7fe76921a2e2ebd93da98aae124048d1
charles delannoy <charles.delannoy@hei.fr>
2021-12-12 22:56:34 +0100

    plants seeding v1

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,37 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'open-uri'
+require 'nokogiri'
+
+puts 'start seeding...'
+puts ''
+
+puts 'Remove all plants...'
+Plant.destroy_all
+puts 'Plants removed !'
+
+puts ''
+
+base_url = "https://plantespourtous.co/fiches-entretien/"
+
+base_html_doc = Nokogiri::HTML(open(base_url).read)
+
+plants_items = base_html_doc.search('.item-container')
+
+browser = Ferrum::Browser.new()
+
+plants_items.each do |plant_item|
+  name = plant_item.search('.item-title').text.strip
+  url = plant_item.search('a').attribute('href').value
+  browser.goto url
+  description = browser.at_css('.ppt-care-list li:nth-child(n+2) p').text
+  Plant.create!(name: name, description: description)
+
+  puts "Added a new plant: '#{name}' Yay!"
+  puts "There is now #{Plant.count} plants"
+  puts ''
+end
+
+puts 'SUCCESS!'
