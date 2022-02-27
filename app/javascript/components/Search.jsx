79997a5d023753a28fdda8a3ff16d2e6be071f77
charles delannoy <charles.delannoy@hei.fr>
2022-02-27 19:28:28 +0100

    search a plants by name

--- app/javascript/components/Search.jsx
+++ app/javascript/components/Search.jsx
@@ -1,22 +1,48 @@
-import React, { useState, useContext, useEffect } from 'react'
-import { SessionContext } from './contexts/SessionContext'
+import React, { useState, useEffect } from 'react'
 import Header from './shared/Header'
 import Footer from './shared/Footer'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faSearch } from '@fortawesome/free-solid-svg-icons'
 import axios from 'axios'
+import SearchBar from './SearchBar'
 
 const Search = () => {
   const [plants, setPlants] = useState([])
+  const [filteredPlants, setFilteredPlants] = useState(plants)
+  const [loaded, setLoaded] = useState(false)
+  const [searchValue, setSearchValue] = useState('')
+
+  const sortByName = (listToSort) => {
+    listToSort.sort((a, b) => a.name.localeCompare(b.name))
+  }
+
+  const updatePlants = () => {
+    setFilteredPlants(
+      plants.filter(
+        plant => plant.name.toLowerCase().includes(searchValue.toLowerCase())
+      )
+    )
+  }
 
   useEffect(() => {
-    axios.get('/plants')
-      .then(response => {
+    const retrievePlants = async () => {
+      if (!loaded) {
+        const response = await axios.get('/plants')
+        sortByName(response.data.plants)
         setPlants(response.data.plants)
-      })
+        setFilteredPlants(response.data.plants)
+        setLoaded(true)
+      }
+    }
+    retrievePlants()
   })
 
-  const display_plants = plants.map((plant) => {
+  useEffect(() => {
+    updatePlants()
+    console.log(filteredPlants)
+  }, [searchValue])
+
+  const display_plants = filteredPlants.map((plant) => {
     return (
     <div key= {plant.id} className='plant-card'>
       <h3>{plant.name}</h3>
@@ -31,7 +57,10 @@ const Search = () => {
       <div className="home-content">
         <h1 className='main-title'>Search a plant</h1>
         <div className='search-bar'>
-          <input></input>
+          <SearchBar
+            value={searchValue}
+            setValue={setSearchValue}>
+          </SearchBar>
           <FontAwesomeIcon icon={faSearch} />
         </div>
         {display_plants}
